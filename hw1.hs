f9bb0a8d9a9a13cf066aabd43e9ab6fa646d596c
cjurden <colejurden@gmail.com>
2015-09-23 13:19:01 -0500

    fixed blank space problem

--- hw1.hs
+++ hw1.hs
@@ -1,75 +1,69 @@
 main = do
 
-	let s1 = (cypher "HELLO THERE")::String
-	let s2 = (cypher "WORLD STAR")::String
-	let s3 = (cypher "HASKELL IS GREAT")::String
-	let s4 = (cypher "JAYHAWKS RULE")::String
-	putStrLn (s1 ++ s2 ++ s3 ++ s4)
-	let s5 = (cypher s1)::String  
-	let s6 = (cypher s2)::String 
-	let s7 = (cypher s3)::String 
-	let s8 = (cypher s4)::String 
-	putStrLn (s5 ++ s6 ++ s7 ++ s8)
+	let s1 = (cypher "THE QUICK BROWN FOX JUMPED OVER THE LAZY DOG")::String
+	putStrLn (s1)
+	let s2 = (cypher s1)::String
+	putStrLn (s2)
 
 f x = case x of 
-	'A' -> 1
-	'B' -> 2
-	'C' -> 3
-	'D' -> 4
-	'E' -> 5
-	'F' -> 6
-	'G' -> 7
-	'H' -> 8
-	'I' -> 9
-	'J' -> 10
-	'K' -> 11
-	'L' -> 12
-	'M' -> 13
-	'N' -> 14
-	'O' -> 15
-	'P' -> 16
-	'Q' -> 17
-	'R' -> 18
-	'S' -> 19
-	'T' -> 20
-	'U' -> 21
-	'V' -> 22
-	'W' -> 23
-	'X' -> 24
-	'Y' -> 25
-	'Z' -> 26
-	_ -> 0
+	'A' -> 0 
+	'B' -> 1 
+	'C' -> 2 
+	'D' -> 3 
+	'E' -> 4 
+	'F' -> 5 
+	'G' -> 6 
+	'H' -> 7 
+	'I' -> 8 
+	'J' -> 9 
+	'K' -> 10
+	'L' -> 11
+	'M' -> 12
+	'N' -> 13
+	'O' -> 14
+	'P' -> 15
+	'Q' -> 16
+	'R' -> 17
+	'S' -> 18
+	'T' -> 19
+	'U' -> 20
+	'V' -> 21
+	'W' -> 22
+	'X' -> 23
+	'Y' -> 24
+	'Z' -> 25
+	' ' -> 26
 
-y x = (x+13) `mod` 26
+y x = if x < 26 then (x+13) `mod` 26 else x
 
 g x = case x of 
-	1-> 'A'
-	2-> 'B'
-	3-> 'C'
-	4-> 'D'
-	5-> 'E'
-	6-> 'F'
-	7-> 'G'
-	8-> 'H'
-	9-> 'I'
-	10->'J'
-	11->'K'
-	12->'L'
-	13->'M'
-	14->'N'
-	15->'O'
-	16->'P'
-	17->'Q'
-	18->'R'
-	19->'S'
-	20->'T'
-	21->'U'
-	22->'V'
-	23->'W'
-	24->'X'
-	25->'Y'
-	26->'Z'
-	_ -> ' '
+	0 -> 'A'
+	1 -> 'B'
+	2 -> 'C'
+	3 -> 'D'
+	4 -> 'E'
+	5 -> 'F'
+	6 -> 'G'
+	7 -> 'H'
+	8 -> 'I'
+	9 ->'J'
+	10->'K'
+	11->'L'
+	12->'M'
+	13->'N'
+	14->'O'
+	15->'P'
+	16->'Q'
+	17->'R'
+	18->'S'
+	19->'T'
+	20->'U'
+	21->'V'
+	22->'W'
+	23->'X'
+	24->'Y'
+	25->'Z'
+	26 -> ' '
 
 cypher s = map g (map y (map f s))
 
